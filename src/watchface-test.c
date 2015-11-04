ab047bcc6708094022208299796f8f3178b78765
tiao564 <tiao564@hotmail.com>
2015-11-04 13:14:53 -0500

    Working

--- src/watchface-test.c
+++ src/watchface-test.c
@@ -51,7 +51,19 @@ int main(void){
   //Initiation//
   //////////////
 static void inbox_received_callback(DictionaryIterator *iterator, void *context){
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
   
+  Tuple *temp_tuple = dict_find(iterator, KEY_TEMPERATURE);
+  Tuple *conditions_tuple = dict_find(iterator, KEY_CONDITIONS);
+  
+  if(temp_tuple && conditions_tuple){
+    snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)temp_tuple->value->int32);
+    snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer),"%s, %s", temperature_buffer, conditions_buffer);
+    text_layer_set_text(s_weather_layer, weather_layer_buffer);
+  }
 }
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context){
@@ -174,6 +186,17 @@ static void main_window_unload(Window *window){
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
   update_time();
+ if(tick_time->tm_min %30 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+    
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+    
+    // Send the message
+    app_message_outbox_send();
+  }
 }
 
 static void update_time(){
